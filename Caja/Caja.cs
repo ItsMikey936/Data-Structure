ecb7fc447100d307582cec543e992b80b1337d56
MonarchDev <miguelarellan117@gmail.com>
2025-10-07 20:06:42 -0600

    Add Generic pile lab

---
+++ Caja/Caja.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caja
+{
+    internal class Caja<T>
+    {
+        // Propiedad que almacena el valor
+        public T Contenido { get; set; }
+
+        // Constructor que inicializa el contenido
+        public Caja(T valor)
+        {
+            Contenido = valor;
+        }
+
+        // Método para mostrar el contenido en consola
+        public void MostrarContenido()
+        {
+            Console.WriteLine($"Contenido de la caja: {Contenido}");
+        }
+
+    }
+}
---
+++ Caja/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prueba con tipo string
+            Caja<string> cajaTexto = new Caja<string>("¡Heeeey muy buenass!");
+            cajaTexto.MostrarContenido();
+
+            // Prueba con tipo int
+            Caja<int> cajaNumero = new Caja<int>(2025);
+            cajaNumero.MostrarContenido();
+
+        }
+    }
+}
