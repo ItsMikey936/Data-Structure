564364003bd6db56af5d672261de3e290c5e3fee
MonarchDev <miguelarellan117@gmail.com>
2025-10-07 20:01:58 -0600

    Add Generic Class <T>

---
+++ Almacen/Almacen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen
+{
+    internal class Almacen<T>
+    {
+        // Propiedad que guarda un valor de cualquier tipo
+        public T Elemento { get; set; }
+
+        // Constructor opcional para inicializar el valor
+        public Almacen(T valor)
+        {
+            Elemento = valor;
+        }
+
+        // Método para mostrar el valor en consola
+        public void MostrarElemento()
+        {
+            Console.WriteLine($"Elemento almacenado: {Elemento}");
+        }
+
+    }
+}
---
+++ Almacen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Almacen<string> almacenTexto = new Almacen<string>("¡Hola!");
+
+            // Mostrar el valor guardado
+            almacenTexto.MostrarElemento();
+
+        }
+    }
+}
